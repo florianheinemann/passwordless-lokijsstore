a47cea0c7c9ce0ce80b3df0e66f939efe9828e40
Florian Heinemann <florian.heinemann@gmail.com>
2016-11-12 19:35:07 +0100

    ADD test to check that hashes are really salted

--- test/lokijsstore.js
+++ test/lokijsstore.js
@@ -120,31 +120,42 @@ describe('Specific tests', function() {
 			});
 	})
 
-	// it('should store tokens not only hashed but also salted', function (done) {
-	// 	var store = TokenStoreFactory();
-	// 	var token = uuid.v4();
-	// 	var uid = chance.email();
-	// 	store.storeOrUpdate(token, uid, 
-	// 		1000*60, 'http://' + chance.domain() + '/page.html', 
-	// 		function() {
-	// 			MongoClient.connect(testUri, function(err, db) {
-	// 				db.collection('passwordless-token', function(err, collection) {
-	// 					collection.findOne({uid: uid}, function(err, item) {
-	// 						var hashedToken1 = item.hashedToken;
-	// 						store.clear(function() {
-	// 							store.storeOrUpdate(token, uid, 
-	// 								1000*60, 'http://' + chance.domain() + '/page.html', 
-	// 								function() {
-	// 									collection.findOne({uid: uid}, function(err, item) {
-	// 										var hashedToken2 = item.hashedToken;
-	// 										expect(hashedToken2).to.not.equal(hashedToken1);
-	// 										done();
-	// 									});
-	// 								});
-	// 						})
-	// 					});
-	// 				});
-	// 			})
-	// 		});
-	// })
+	it('should store tokens not only hashed but also salted', function (done) {
+		var store = TokenStoreFactory();
+		var token = uuid.v4();
+		var uid = chance.email();
+		store.storeOrUpdate(token, uid, 
+			1000*60, 'http://' + chance.domain() + '/page.html', 
+			function() {
+
+				var db = new Loki(testFile);
+				db.loadDatabase({}, function() {
+					var collection = db.getCollection('passwordless-token');
+					if(collection) {
+						var result = collection.findOne({uid: uid}); 
+						expect(result).to.exist;
+						var hashedToken1 = result.hashedToken;
+
+						store = TokenStoreFactory();
+						store.clear(function() {
+							store.storeOrUpdate(token, uid, 
+								1000*60, 'http://' + chance.domain() + '/page.html', 
+								function() {
+									var db = new Loki(testFile);
+									db.loadDatabase({}, function() {
+										var collection = db.getCollection('passwordless-token');
+										if(collection) {
+											var result2 = collection.findOne({uid: uid});
+											expect(result2).to.exist;
+											var hashedToken2 = result2.hashedToken;
+											expect(hashedToken2).to.not.equal(hashedToken1);
+											done();
+										}
+									})
+								});
+						})	
+					}
+				})
+			});
+	})
 })
